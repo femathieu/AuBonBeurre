500a485f43ef9c5c58ba9f4ef1c96b12fecda24b
mathieuf <mathieuferron06@gmail.com>
2019-08-22 15:25:12 +0200

    python socket send josn data to server

--- src/app_middleware/main/server.py
+++ src/app_middleware/main/server.py
@@ -3,6 +3,8 @@
 # coding: utf-8
 
 import socket
+import re
+import os
 
 socket = socket.socket(socket.AF_INET, socket.SOCK_STREAM)
 socket.bind(('', 15555))
@@ -10,11 +12,39 @@ socket.bind(('', 15555))
 while True:
         socket.listen(5)
         client, address = socket.accept()
-        print("{} connected".format( address ))
+        print("{} connected".format(address))
 
-        response = client.recv(255)
+        response = client.recv(1024)
+
         if response != "":
-                print(response)
+                print("\n response {} ".format(response))
+
+
+        # clean files
+        files = [f for f in os.listdir('.') if re.match(r'^\d*\.\d*', f)]
+        for f in files:
+           print("'{}' removed".format(f))
+           os.remove(f)
+
+        # create file
+        date = "{}".format(re.search(r'\d*\.\d*', response.decode('utf-8')).group())
+        data = open(date, "w+")
+
+        # fill file
+        responseInString = re.sub(r'\d*\.\d*', '', response.decode('utf-8'))
+        response = str.encode(responseInString)
+        print("{}".format(response.decode('utf-8')))
+        data = ""
+        files = [f for f in os.listdir('.') if re.match(r'^\d*\.\d*', f)]
+        for f in files:
+              data = open(f, "wb")
+        while response:
+              data.write(response)
+              response = client.recv(1024)
+        data.close()
+
+
+
 
 print("Close")
 client.close()
--- src/script/main/code/all_linux00/bin/client.py
+++ src/script/main/code/all_linux00/bin/client.py
@@ -3,6 +3,8 @@
 # coding: utf-8
 
 import socket
+import re
+import os
 
 hote = "172.168.1.2"
 port = 15555
@@ -11,7 +13,24 @@ socket = socket.socket(socket.AF_INET, socket.SOCK_STREAM)
 socket.connect((hote, port))
 print("Connection on {}".format(port))
 
-socket.send("Hey my name is Olivier!")
+# f = open("paramunite_1_1566310765.0.json", "rb")
+f = 0
+date = ""
+files = [f for f in os.listdir('/opt/data/') if re.match(r'.*\.json', f)]
+for file in files:
+    date = "{}".format(re.search(r'\d*\.\d*', file).group())
+    f = open("/opt/data/{}".format(file), "rb")
+
+data = f.read(1024)
+
+socket.send(str.encode(date))
+
+while data:
+    socket.send(data)
+    data = f.read(1024)
+
+
+# socket.send("Hey my name is Olivier!")
 
 print("Close")
 socket.close()
