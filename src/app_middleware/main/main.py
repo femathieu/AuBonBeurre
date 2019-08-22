e47aa1e1f2a03512ef68977f323407044f47447d
Pierre Sid-idris <pierre.sididris@live.fr>
2019-08-22 09:55:28 +0200

    middleware docker conf

--- src/app_middleware/main/main.py
+++ src/app_middleware/main/main.py
@@ -1,5 +1,8 @@
-import json;
-import os;
+import json
+import os
+import re
+import MySQLdb
+
 
 files = [f for f in os.listdir('.') if re.match(r'.*\.json', f)]
 
@@ -8,3 +11,8 @@ for f in files:
         with open(f) as json_file:
             data = json.load(json_file)
             print(data)
+
+
+conn=MySQLdb.connect(host='database', user='root', passwd='9zknCUd,789ZyUu:')
+cursor=conn.cursor()
+cursor.execute('CREATE DATABASE pierrrolastico')
